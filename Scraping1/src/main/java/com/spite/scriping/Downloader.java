6731e8beb5a93125e77587a06855b755894591f2
spite400k <spite400k@gmail.com>
2018-05-22 23:21:02 +0900

    first

---
+++ Scraping1/src/main/java/com/spite/scriping/Downloader.java
@@ -0,0 +1,29 @@
+package com.spite.scriping;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.integration.annotation.InboundChannelAdapter;
+import org.springframework.integration.annotation.MessageEndpoint;
+import org.springframework.integration.annotation.Poller;
+import org.springframework.web.client.RestTemplate;
+
+@MessageEndpoint
+public class Downloader {
+	@Autowired
+	private CrawlerConfig config;
+
+	@Autowired
+	private RestTemplate template;
+
+	@InboundChannelAdapter(value = "channel1", poller = @Poller("downloadTrigger"))
+	public ResponseEntity<String> download() {
+		String url = config.getUrl();
+		try {
+			SSLUtil.turnOffSslChecking();
+		}catch (Exception e){
+
+		}
+		ResponseEntity<String> entity = template.getForEntity(url, String.class);
+		return entity;
+	}
+}
